aafff417ce5e2f8344393e6b139e42db8569798f
Anshuman Tiwari <112653339+AnshumanCse@users.noreply.github.com>
2023-02-28 08:12:52 -0600

    Create

---
+++ Scope.js
@@ -0,0 +1,21 @@
+// localScope
+
+// Block scope
+{
+  var a = 2;
+}
+console.log(a)
+
+// function scope
+function FindSum(a,b){
+  let sum = a+b;
+}
+// console.log(FindSum)
+
+// global scope
+let sum = 10;
+function calSum(a,b){
+  sum = a+b;
+  console.log(sum)
+}
+console.log(sum);
